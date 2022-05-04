c1f69a63a1117501b0d3d2324452439151718666
Adrian Diaz <adrian93eh3@gmail.com>
2022-05-04 19:22:09 -0400

    installed axios and imported axios. imported {useState} as well.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>Hello</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
