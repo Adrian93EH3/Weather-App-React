db26604b004e8385bd03bc39859254668c7133b4
Adrian Diaz <adrian93eh3@gmail.com>
2022-05-05 11:18:05 -0400

    removed the base css to start styling/added a url const to contain my api url call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {useState} from "react";
 import axios from "axios";
 
 function App() {
+
+ const url = "https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=ff190912e73db2be11befe0e9575b205"
+
   return (
     <div className="App">
       <header className="App-header">
