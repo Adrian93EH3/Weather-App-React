4a869ccddf4f30f7d5d9196cbeb9ac9a584c683a
Adrian Diaz <adrian93eh3@gmail.com>
2022-05-09 14:07:25 -0400

    added a function searchWeather that will grab the location's data based off an event.key response of the enter button. Also added an input field, will add styling in the next commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,27 @@ function App() {
 
  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ff190912e73db2be11befe0e9575b205`
 
+ const searchWeather = (event) => {
+
+  if (event.key === 'Enter') /* Checking for user hitting the Enter button */ {
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    })
+  }
+
+ }
   return (
     <div className="App">
+    <div className="search">
+      <input 
+      type="text" 
+      value={location} 
+      onChange={event => setLocation(event.target.value)} 
+      placeholder="Search your city's weather here..."
+      onKeyPress={searchWeather}  
+      />
+    </div>
       <div className="container">
         <div className="top">
           <div className="location">
